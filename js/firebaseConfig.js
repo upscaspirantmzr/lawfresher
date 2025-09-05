d9f4eeb75bf324515943a118de9bf12304a1b092
Dancing bull <72798082+upscaspirantmzr@users.noreply.github.com>
2025-09-05 18:48:50 +0530

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,9 +1,10 @@
 // Firebase Config (replace with your project details)
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "yourproject.firebaseapp.com",
-  projectId: "yourproject",
-  storageBucket: "yourproject.appspot.com",
-  messagingSenderId: "XXXXXX",
-  appId: "XXXXXXXXXX"
-};
+    apiKey: "AIzaSyAbnFwUEduIFg7uC7mqy10ebB-bEFOv-z0",
+    authDomain: "lawfresher.firebaseapp.com",
+    projectId: "lawfresher",
+    storageBucket: "lawfresher.firebasestorage.app",
+    messagingSenderId: "670819580273",
+    appId: "1:670819580273:web:743d354f801d5a92f6ded0",
+    measurementId: "G-PD0NGZ2BR2"
+  };
