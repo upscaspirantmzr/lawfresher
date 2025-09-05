d93a5eb51cabee9ba7a8cddbc74071ce59daf72c
Dancing bull <72798082+upscaspirantmzr@users.noreply.github.com>
2025-09-05 13:09:17 +0530

    Create firebaseConfig.js

---
+++ js/firebaseConfig.js
@@ -0,0 +1,9 @@
+// Firebase Config (replace with your project details)
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "yourproject.firebaseapp.com",
+  projectId: "yourproject",
+  storageBucket: "yourproject.appspot.com",
+  messagingSenderId: "XXXXXX",
+  appId: "XXXXXXXXXX"
+};
