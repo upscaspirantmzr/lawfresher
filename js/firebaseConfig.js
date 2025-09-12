90a8e7a1036001431a6065ba67592d39c107c10e
Dancing bull <72798082+upscaspirantmzr@users.noreply.github.com>
2025-09-12 11:16:56 +0530

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,10 +1,10 @@
-// Firebase Config (replace with your project details)
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-    apiKey: "AIzaSyAbnFwUEduIFg7uC7mqy10ebB-bEFOv-z0",
-    authDomain: "lawfresher.firebaseapp.com",
-    projectId: "lawfresher",
-    storageBucket: "lawfresher.firebasestorage.app",
-    messagingSenderId: "670819580273",
-    appId: "1:670819580273:web:743d354f801d5a92f6ded0",
-    measurementId: "G-PD0NGZ2BR2"
-  };
+  apiKey: "AIzaSyAbnFwUEduIFg7uC7mqy10ebB-bEFOv-z0",
+  authDomain: "lawfresher.firebaseapp.com",
+  projectId: "lawfresher",
+  storageBucket: "lawfresher.firebasestorage.app",
+  messagingSenderId: "670819580273",
+  appId: "1:670819580273:web:743d354f801d5a92f6ded0",
+  measurementId: "G-PD0NGZ2BR2"
+};
